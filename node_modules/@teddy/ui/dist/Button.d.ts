2bb38710276e14d5a1ce9fe9c023b6863b3427cf
Matheus Santos <somathsant@gmail.com>
2025-07-18 23:06:53 -0300

    fix: Remove node_modules from repository

--- node_modules/@teddy/ui/dist/Button.d.ts
+++
@@ -1,6 +0,0 @@
-import { ReactNode, ButtonHTMLAttributes } from 'react';
-type ButtonProps = {
-    children: ReactNode;
-} & ButtonHTMLAttributes<HTMLButtonElement>;
-export declare const Button: ({ children, ...props }: ButtonProps) => import("react/jsx-runtime").JSX.Element;
-export {};
--- node_modules/@teddy/ui/dist/Button.js
+++
@@ -1,2 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-export const Button = ({ children, ...props }) => (_jsx("button", { ...props, className: "px-4 py-2 bg-blue-600 text-white rounded", children: children }));
